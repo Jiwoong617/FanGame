e5b81d702944f0aa937d70ed2cf77f3517ee16af
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2026-03-08 20:56:35 +0900

    20260308 Fix ToolTip
    - 툴팁 화면에 안 잘리게 수정

--- Assets/Scripts/UI/Inventory/TooptipUI.cs
+++ Assets/Scripts/UI/Inventory/TooptipUI.cs
@@ -15,6 +15,9 @@ public class TooltipUI : UI_Base
     private TMP_Text nameText;
     private TMP_Text descText;
     private TMP_Text flavorText;
+    RectTransform rectTransform;
+    Canvas parentCanvas;
+    RectTransform canvasRect;
 
     protected override void Init()
     {
@@ -30,6 +33,10 @@ public class TooltipUI : UI_Base
         nameText = Get<TMP_Text>(Texts.NameText);
         descText = Get<TMP_Text>(Texts.DescText);
         flavorText = Get<TMP_Text>(Texts.FlavorText);
+
+        rectTransform = GetComponent<RectTransform>();
+        parentCanvas = GetComponentInParent<Canvas>();
+        canvasRect = parentCanvas.GetComponent<RectTransform>();
         Hide();
     }
 
@@ -41,6 +48,36 @@ public class TooltipUI : UI_Base
 
         transform.position = slotPosition;
         Show();
+
+        UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+        if (parentCanvas != null)
+        {
+            Vector3[] corners = new Vector3[4];
+            rectTransform.GetWorldCorners(corners);
+            
+            Vector3[] canvasCorners = new Vector3[4];
+            canvasRect.GetWorldCorners(canvasCorners);
+
+            float minX = corners[0].x;
+            float minY = corners[0].y;
+            float maxX = corners[2].x;
+            float maxY = corners[1].y;
+
+            float cMinX = canvasCorners[0].x;
+            float cMinY = canvasCorners[0].y;
+            float cMaxX = canvasCorners[2].x;
+            float cMaxY = canvasCorners[1].y;
+
+            Vector3 newPos = transform.position;
+
+            if (minX < cMinX) newPos.x += (cMinX - minX);
+            else if (maxX > cMaxX) newPos.x -= (maxX - cMaxX);
+
+            if (minY < cMinY) newPos.y += (cMinY - minY);
+            else if (maxY > cMaxY) newPos.y -= (maxY - cMaxY);
+
+            transform.position = newPos;
+        }
     }
 
     public void HideTooltip()
